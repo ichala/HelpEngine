545378275cdfeb87339e8b0faa2c68a791a6917c
Ali Jendoubi <alijandhd@gmail.com>
2022-12-08 13:29:22 +0100

    Add Search articles method

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,10 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   #content must be present and 5 - 500 chars long
   validates :content, presence: true, length: { minimum: 5, maximum: 500 }
+
+  def self.search_by_title(params)
+    params[:query].blank? ? all.limit(10) : where(
+      "title LIKE ?", "%#{params[:query]}%"
+    )
+  end
 end
