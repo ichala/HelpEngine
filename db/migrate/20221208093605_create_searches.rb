503aec9635cf9ff31876c538149b13c4e9e1d913
Ali Jendoubi <alijandhd@gmail.com>
2022-12-08 16:17:28 +0100

    Add pg_trgm extension

--- db/migrate/20221208093605_create_searches.rb
+++ db/migrate/20221208093605_create_searches.rb
@@ -1,4 +1,5 @@
 class CreateSearches < ActiveRecord::Migration[7.0]
+  enable_extension 'pg_trgm'
   def change
     create_table :searches do |t|
       t.string :user_ip, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_08_093605) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
