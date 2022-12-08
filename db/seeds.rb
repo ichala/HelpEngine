1c208a02d37273d0a28d519aa920f27bf62e595b
Ali Jendoubi <alijandhd@gmail.com>
2022-12-08 10:46:22 +0100

    Create fake data seeds for article

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Generate 10 articles 
+10.times do 
+    Article.create!(title:Faker::Lorem.sentence(word_count: 3),content: Faker::Lorem.paragraph(sentence_count: 3, supplemental: false, random_sentences_to_add: 4))
+end
+
